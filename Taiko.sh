fb36faaa605d3e87b4ce85efba3c9c1497fea28d
run-node <160773499+run-node@users.noreply.github.com>
2024-05-11 14:00:15 +0800

    Update Taiko.sh

--- Taiko.sh
+++ Taiko.sh
@@ -300,30 +300,31 @@ function change_rpc() {
   cd $HOME
   cd simple-taiko-node
 
-  rpc_list = [
-    "http://198.244.201.79:9876",
-    "https://prover-hekla.taiko.tools",
-    "https://prover2-hekla.taiko.tools",
-    "http://taiko-a7-prover.zkpool.io",
-    "http://146.59.55.26:9876",
-    "http://kenz-prover.hekla.kzvn.xyz:9876",
-    "http://hekla.stonemac65.xyz:9876",
-    "http://51.91.70.42:9876",
-    "http://taiko.web3crypt.net:9876",
-    "http://148.113.17.127:9876",
-    "http://hekla.prover.taiko.coinblitz.pro:9876",
-    "http://taiko-testnet.m51nodes.xyz:9876",
-    "http://148.113.16.26:9876",
-    "http://51.161.118.103:9876",
-    "http://162.19.98.173:9876",
-    "http://49.13.215.95:9876",
-    "http://49.13.143.184:9876",
-    "http://49.13.210.192:9876",
-    "http://159.69.242.22:9876",
-    "http://49.13.69.238:9876",
-    "http://taiko.guru:9876",
+  rpc_list=(
+    "http://198.244.201.79:9876"
+    "https://prover-hekla.taiko.tools"
+    "https://prover2-hekla.taiko.tools"
+    "http://taiko-a7-prover.zkpool.io"
+    "http://146.59.55.26:9876"
+    "http://kenz-prover.hekla.kzvn.xyz:9876"
+    "http://hekla.stonemac65.xyz:9876"
+    "http://51.91.70.42:9876"
+    "http://taiko.web3crypt.net:9876"
+    "http://148.113.17.127:9876"
+    "http://hekla.prover.taiko.coinblitz.pro:9876"
+    "http://taiko-testnet.m51nodes.xyz:9876"
+    "http://148.113.16.26:9876"
+    "http://51.161.118.103:9876"
+    "http://162.19.98.173:9876"
+    "http://49.13.215.95:9876"
+    "http://49.13.143.184:9876"
+    "http://49.13.210.192:9876"
+    "http://159.69.242.22:9876"
+    "http://49.13.69.238:9876"
+    "http://taiko.guru:9876"
     "http://taiko.donkamote.xyz:9876"
-]
+  )
+
 
   rpc_string=""
 
